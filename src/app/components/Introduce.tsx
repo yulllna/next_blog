1b99c0d5561cc06bd67e9aa99716c99c0611f349
Yuri Na <soceu@naver.com>
2024-04-07 21:46:18 +0900

    프로필 컴포넌트 라우터 연결

--- src/app/components/Introduce.tsx
+++ src/app/components/Introduce.tsx
@@ -1,9 +1,11 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
-
-// import profileImage from '/images/profile.jpeg';
+import { useRouter } from 'next/navigation';
 
 const Introduce = () => {
+    const router = useRouter();
+
     return (
         <div className='flex justify-center flex-col items-center pb-8'>
             <div className='w-[150px] h-[150px] mb-2'>
@@ -11,7 +13,7 @@ const Introduce = () => {
             </div>
             <p className='font-bold text-xl'>Hi, I'm Yuri</p>
             <p className='text-center text-sm'>Web front-end engineer<br/>지식을 모아가는 주니어</p>
-            <button className='bg-orange-400 rounded-2xl px-2 py-1 text-sm mt-2'>Contact Me</button>
+            <button className='bg-orange-400 rounded-2xl px-2 py-1 text-sm mt-2' onClick={() => {router.push('/contact')}}>Contact Me</button>
         </div>
     );
 };
